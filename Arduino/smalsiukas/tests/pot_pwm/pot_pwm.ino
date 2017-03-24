659ce73a7f1c933206fe453d058558634d2c1a74
ejs-ejs <ejs@arches.lt>
2017-03-24 20:59:24 +0200

    Cleanup
    SMT GDS FET changed to more common GSD FET;
    OSHW and CC symbols added to copper layer;
    Gerbers updated and sent for manufacturing;
    
    Typeo fixed in Arduino code

--- Arduino/smalsiukas/tests/pot_pwm/pot_pwm.ino
+++ Arduino/smalsiukas/tests/pot_pwm/pot_pwm.ino
@@ -1,6 +1,6 @@
 
-#include "../../../include/SteeringPot.h"
-#include "..\include\TestPot.h"
+#include <SteeringPot.h>
+#include <TestPot.h>
 
 int potPin = A0;    // select the input pin for the potentiometer
 int drivingPin = 6;    // pwm goes to
@@ -8,22 +8,24 @@ int ledPin = 13;      // select the pin for the LED
 long pValue;  // variable to store the previous value from the steering wheel
 
 long tmp;
+TestPot refPot(potPin);
+SteeringPot steeringWheel(drivingPin);
 
+  
 void setup() {
   // put your setup code here, to run once:
   // initialize serial communications at 9600 bps:
   Serial.begin(115200);
   delay(300);
   
-  TestPot refPot(potPin);
+
 
   Serial.print("Joystick correction = ");
-  Serial.println(refPot.getCorrection);
+  Serial.println(refPot.getCorrection());
   
-  SteeringPot steeringWheel(drivingPin);
 
   Serial.print("Steering correction = ");
-  Serial.println(steeringWheel.getCorrection);
+  Serial.println(steeringWheel.getCorrection());
   
   // declare the ledPin as an OUTPUT:
   pinMode(ledPin, OUTPUT);
@@ -42,9 +44,9 @@ void loop() {
       Serial.print("\t current angle = ");
       Serial.print(steeringWheel.getAngle());
       Serial.print("\n \tPotentiometer correction = ");
-      Serial.println(refPot.getCorrection);
+      Serial.println(refPot.getCorrection());
     }
-    delay(abs(refPot.grads());
+    delay(abs(refPot.grads(refPot.value())));
     // turn the ledPin off:
     digitalWrite(ledPin, LOW);
 }
